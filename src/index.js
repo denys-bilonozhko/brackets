693771f624ea430e5d67fc2613ab14dbed0a4c6b
denys-bilonozhko <deformix@gmail.com>
2021-01-20 22:06:31 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let bracketPair = '';
+
+  while (str.length) {
+    let stopCheck = true;
+
+    for (let i of bracketsConfig) {
+      bracketPair = i[0] + i[1];
+
+      if (str.includes(bracketPair)) {
+        str = str.replace(bracketPair, '');
+        stopCheck = false;
+      }
+    }
+
+    if (stopCheck) {
+      break;
+    }
+  }
+
+  if (str.length) {
+    return false;
+  }
+
+  return true;
+};
